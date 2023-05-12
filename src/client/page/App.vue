75e43f1faed3f7c3ee3dd24c7c84f76f224c17b9
plq <562714485@qq.com>
2023-05-12 22:32:45 +0800

    feat: add platform display

--- src/client/page/App.vue
+++ src/client/page/App.vue
@@ -4,12 +4,12 @@
     <header class="header">
       <div class="title">
         <svg t="1681373640980" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-          p-id="6973" width="20" height="20" style="margin-right: 16px">
+          p-id="6973" width="36" height="36" style="margin-right: 16px">
           <path
             d="M510.272 0q137.152-1.152 257.728 68.576 132.576 76.576 201.152 212.576l-424-22.272q-91.424-5.152-168 42.56t-105.728 131.136l-157.728-242.272q73.152-90.848 177.728-140.288t218.848-50.016zM83.424 231.424l192.576 378.848q41.152 81.728 120.576 124t167.424 25.728l-131.424 257.728q-121.152-18.848-220-90.016t-155.712-180.576-56.864-235.136q0-152.576 83.424-280.576zM989.728 328q33.152 85.728 34.016 177.44t-27.712 174.848-87.424 155.424-140.576 119.712q-131.424 76-284.576 68l231.424-356q50.272-74.848 47.136-166.016t-60.864-158.56zM512 339.424q71.424 0 122.016 50.56t50.56 122.016-50.56 122.016-122.016 50.56-122.016-50.56-50.56-122.016 50.56-122.016 122.016-50.56z"
             p-id="6974"></path>
         </svg>
-        Chrome Remote Debuger
+        Devtools Remote Debugger
       </div>
       <div class="operation">
         <div class="debug-demo" @click="openDemo">
@@ -58,7 +58,7 @@ body {
 
 .header {
   height: 64px;
-  border-bottom: 1px solid #eee;
+  border-bottom: 3px solid #eee;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -66,11 +66,16 @@ body {
   margin-bottom: 32px;
 }
 
+.header .title {
+  font-size: 24px;
+}
+
 .header .title,
 .operation {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  font-weight: bold;
 }
 
 .debug-demo {
